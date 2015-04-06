4c6635d80d03f521eeac6f86a838fb42ece19a28
Allen Poon <a1b2c3d42003hongkong@gmail.com>
2015-04-06 15:28:43 +0800

    fixed bug -_-

--- dropbox/python/dropboxapi.py
+++ dropbox/python/dropboxapi.py
@@ -19,7 +19,7 @@ class DropboxAPI:
 		for option in options:
 			try:
 				dict1[option] = Config.get(section, option)
-				if dict1[option] == -1:
+				#if dict1[option] == -1:
 					#DebugPrint("skip: %s" % option)
 			except:
 				#print("exception on %s!" % option)
